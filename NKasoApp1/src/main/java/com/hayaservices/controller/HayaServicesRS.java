bbd36fa8700eecc94b2710c4610cb3fb296e3a58
DerAlk <112363395+DerAlk@users.noreply.github.com>
2022-11-03 20:32:39 -0400

    Newly updated

--- NKasoApp1/src/main/java/com/hayaservices/controller/HayaServicesRS.java
+++ NKasoApp1/src/main/java/com/hayaservices/controller/HayaServicesRS.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hayaservices.dao.ApartmentsDao;
+import com.hayaservices.dao.ReservationsDao;
+import com.hayaservices.dao.RoomsDao;
+import com.hayaservices.dao.TransfersDao;
 import com.hayaservices.entities.Apartments;
+import com.hayaservices.entities.Reservations;
+import com.hayaservices.entities.Rooms;
+import com.hayaservices.entities.Transfers;
 
       @RestController 
       @Component
@@ -15,8 +21,8 @@ import com.hayaservices.entities.Apartments;
 	
 	   @Autowired
 	    private ApartmentsDao dao;
-	    @GetMapping("apartments/ {aptId} ")
-	    public Apartments find(@PathVariable  int aptId) {
+	    @GetMapping("/apartments/{aptId}")
+	    public Apartments findApartment(@PathVariable  int aptId) {
 		java.util.Optional<Apartments> opt = dao.findById(aptId);
 		Apartments a = new Apartments();
 		if (opt.isPresent())  {
@@ -24,6 +30,48 @@ import com.hayaservices.entities.Apartments;
 	     }
 	
 	   return a;
+		
+	    }
+	    
+	    @Autowired
+	    private ReservationsDao rdao;
+	    @GetMapping("/Reservations/{reservationId}")
+	    public Reservations findReservation(@PathVariable  int reservationId) {
+		java.util.Optional<Reservations> opt = rdao.findById(reservationId);
+		Reservations r = new Reservations();
+		if (opt.isPresent())  {
+		r =opt.get();
+	     }
+	
+	   return r;
+		
+	    }
+	    
+	    @Autowired
+	    private RoomsDao rmdao;
+	    @GetMapping("/rooms/{aptId}")
+	    public Rooms findRoom(@PathVariable  int roomId) {
+		java.util.Optional<Rooms> opt = rmdao.findById(roomId);
+		Rooms o = new Rooms();
+		if (opt.isPresent())  {
+			o =opt.get();
+	     }
+	
+	   return o;
+		
+	    }
+	    
+	    @Autowired
+	    private TransfersDao tdao;
+	    @GetMapping("/transfers/{transferId}")
+	    public Transfers findTransafers(@PathVariable  int transferId) {
+		java.util.Optional<Transfers> opt = tdao.findById(transferId);
+		Transfers t = new Transfers();
+		if (opt.isPresent())  {
+		t =opt.get();
+	     }
+	
+	   return t;
 		
 	    }
 	
